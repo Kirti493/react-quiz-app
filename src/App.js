2d51d1c85fc4052b2688519e626bba07d98f5631
Kirti493 <kirtisri493@gmail.com>
2021-04-05 23:24:59 +0530

    2 files modified

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import React from "react";
 import "./style.css";
 
+const API_URL =
+  "https://opentdb.com/api.php?amount=10&category=14&difficulty=easy";
+
 export default function App() {
   return (
-    <div>
-      <h1>Hello</h1>
-      <p>Start </p>
+    <div className="container">
+      <div>This is where we're going to have the question.</div>
+      <div>Answer 1</div>
+      <div>Answer 2</div>
+      <div>Answer 3</div>
+      <div>Answer 4</div>
     </div>
   );
 }
