fe445875ef4e1975a34642977fba02b23e05dda1
Kirti493 <kirtisri493@gmail.com>
2021-04-07 00:49:44 +0530

    1 file modified

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import React from "react";
+import React, { useState } from "react";
 import "./style.css";
 
 const API_URL =
   "https://opentdb.com/api.php?amount=10&category=14&difficulty=easy";
 
 export default function App() {
+  const [questions, setQuestions] = useState([]);
   return (
     <div className="container">
       <div className="bg-white text-purple-800 p-10 rounded-lg shadow-md">
@@ -12,17 +13,17 @@ export default function App() {
           1. This is where we're going to have the question.
         </h2>
       </div>
-      <div className="flex flex-wrap mt-4 justify-around">
-        <button className="bg-white w-5/12 p-4 text-purple-800 font-semibold rounded shadow mb-4">
+      <div className="grid grid-cols-2 gap-6 mt-6">
+        <button className="bg-white p-4 text-purple-800 font-semibold rounded shadow">
           Answer 1
         </button>
-        <button className="bg-white w-5/12 p-4 text-purple-800 font-semibold rounded shadow mb-4">
+        <button className="bg-white p-4 text-purple-800 font-semibold rounded shadow">
           Answer 2
         </button>
-        <button className="bg-white w-5/12 p-4 text-purple-800 font-semibold rounded shadow mb-4">
+        <button className="bg-white p-4 text-purple-800 font-semibold rounded shadow">
           Answer 3
         </button>
-        <button className="bg-white w-5/12 p-4 text-purple-800 font-semibold rounded shadow mb-4">
+        <button className="bg-white p-4 text-purple-800 font-semibold rounded shadow">
           Answer 4
         </button>
       </div>
