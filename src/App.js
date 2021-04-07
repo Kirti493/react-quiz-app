09ccdbe45be290c2bd21baffcebd2885f9449ab4
Kirti493 <kirtisri493@gmail.com>
2021-04-07 17:19:48 +0530

    1 file modified, 1 added

--- src/App.js
+++ src/App.js
@@ -17,25 +17,8 @@ export default function App() {
 
   return questions.length > 0 ? (
     <div className="container">
-      <div className="bg-white text-purple-800 p-10 rounded-lg shadow-md">
-        <h2 className="text-2xl">{questions[0].question}</h2>
-      </div>
-      <div className="grid grid-cols-2 gap-6 mt-6">
-        <button className="bg-white p-4 text-purple-800 font-semibold rounded shadow">
-          {questions[0].correct_answer}
-        </button>
-        <button className="bg-white p-4 text-purple-800 font-semibold rounded shadow">
-          {questions[0].incorrect_answers[0]}
-        </button>
-        <button className="bg-white p-4 text-purple-800 font-semibold rounded shadow">
-          {questions[0].incorrect_answers[1]}
-        </button>
-        <button className="bg-white p-4 text-purple-800 font-semibold rounded shadow">
-          {questions[0].incorrect_answers[2]}
-        </button>
-      </div>
-    </div>
+
   ) : (
-    <h1>Hey bro... we're loading!</h1>
+    <h2 className="text-2xl text-white font-bold">Loading....</h2>
   );
 }
